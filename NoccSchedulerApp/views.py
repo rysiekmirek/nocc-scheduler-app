648c72a1004a0c414265ffe1793047cd40f3e67a
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-12 00:27:09 +0200

    tour history

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -8,8 +8,10 @@ def schedule_tour(request):
     if request.method == 'POST':
         form = TourForm(request.POST)
         form.save()
-        form = TourForm()
     else:
-        form = TourForm()
-    return render (request, "schedule-tour.html", {'form': form} )
+        context={
+         'tours': Tour.objects.all().order_by('-created')
+        }
+    form = TourForm()
+    return render (request, "schedule-tour.html", {'form': form} , context=context)
     
