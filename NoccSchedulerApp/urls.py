6199b4a28c900ee1f09225103874bfdc5ca3603a
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-11 03:25:57 +0200

    static

--- NoccSchedulerApp/urls.py
+++ NoccSchedulerApp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schedule-tour/', views.schedule_tour, name = 'schedule-tour' ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
