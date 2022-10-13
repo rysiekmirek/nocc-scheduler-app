1ae76ae1d81e834bde94cc2d2c46903d28779692
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-13 02:37:25 +0200

    no mail

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -9,13 +9,13 @@ def schedule_tour(request):
     if request.method == 'POST':
         form = TourForm(request.POST)
         form.save()
-        send_mail(
+        """send_mail(
         '[NOCC-Tour-Scheduler]',
         'New tour was scheduled.',
         'nocc-tour-scheduler@srv30945.seohost.com.pl',
         ['rysiekmirek@gmail.com'],
         fail_silently=False,
-        )
+        )"""
     form = TourForm()
     context={
         'tours': Tour.objects.all().order_by('date'),
