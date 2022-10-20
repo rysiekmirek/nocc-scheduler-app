67c1811a116df89513f8dc21f0cda24f011a298a
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-20 02:47:24 +0200

    3

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -20,7 +20,7 @@ def main(request):
         subject = '[NOCC-Tour-Scheduler] - New Tour " ' + tour_data['tour_name'] + " \" was scheduled"
         from_email = 'nocc-tour-scheduler@akamai.com'
         to = ['rysiekmirek@gmail.com']
-        html_content = '<h2>Hi '+ tour_data.['requestor_name'] + ',</h2><br><h3>Tour details:</h3>'
+        html_content = '<h2>Hi '+ tour_data['requestor_name'] + ',</h2><br><h3>Tour details:</h3>'
         for key, data in tour_data.items():
             html_content += "<b>" + str(key) + "</b> : "
             html_content += "<i>" + str(data) + "</i><br>"
