4e1a41b5843df8e526bdd85212a066c82bdd3605
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-20 02:36:58 +0200

    3

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -17,7 +17,7 @@ def main(request):
             dbentry.id = uuid_value
             dbentry.save()
         tour_data = Tour.objects.filter(id=uuid_value).values()[0]
-        tour_data2 = Tour.objects.filter(id=uuid_value)[0]
+        tour_data2 = Tour.objects.filter(id=uuid_value)
         for data in tour_data2:
             print (data)
         subject = '[NOCC-Tour-Scheduler] - New Tour " ' + tour_data['tour_name'] + " \" was scheduled"
