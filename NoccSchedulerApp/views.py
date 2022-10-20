e4c8fbd831e0e29531dae72ba17fd3e5c6fd89d6
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-20 02:32:45 +0200

    t2

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -17,7 +17,8 @@ def main(request):
             dbentry.id = uuid_value
             dbentry.save()
         tour_data = Tour.objects.filter(id=uuid_value).values()[0]
-
+        tour_data2 = Tour.objects.filter(id=uuid_value)
+        print (tour_data2)
         subject = '[NOCC-Tour-Scheduler] - New Tour " ' + tour_data['tour_name'] + " \" was scheduled"
         from_email = 'nocc-tour-scheduler@akamai.com'
         to = ['rysiekmirek@gmail.com']
