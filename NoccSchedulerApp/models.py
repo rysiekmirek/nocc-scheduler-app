2be07fcd670eee9028dbddd66073420361d780f0
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-23 19:19:50 +0200

    status requested

--- NoccSchedulerApp/models.py
+++ NoccSchedulerApp/models.py
@@ -32,7 +32,11 @@ class Tour(models.Model):
     date = models.DateField()
     start_time = models.TimeField(help_text="use local time for location")
     end_time = models.TimeField(help_text="use local time for location")
-    approved = models.BooleanField(default=False)
+    STATUS_CHOICES= [
+        ('Requested', 'Requested'),
+        ('Approved', 'Approved'),
+    ]
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='Requested')
     PERSON_ASSIGNED_CHOICES = [
         ('None', 'None'),
         ('Adam', 'Adam'),
