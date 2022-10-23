ed37dc26380be3d82055f20dcbfa9242b73f8e32
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-23 22:43:15 +0200

    44

--- NoccSchedulerApp/forms.py
+++ NoccSchedulerApp/forms.py
@@ -50,23 +50,4 @@ class TourFormEdit(ModelForm):
           'feedback': Textarea(attrs={'rows':1, 'cols':50, 'readonly': 'readonly'}),
           'id': TextInput(attrs={'readonly': 'readonly'}),
         }
-    def clean(self):
-        cleaned_data = super().clean()
-        start_time = cleaned_data.get('start_time')
-        end_time = cleaned_data.get('end_time')
-        if start_time >= end_time:
-            self.add_error('end_time', ValidationError(
-                _('End time has to be after start time'), code='invalid'))
-        for existing_tour in Tour.objects.all():
-            if cleaned_data.get('date') == existing_tour.date:
-                if existing_tour.start_time <= start_time <= existing_tour.end_time:
-                    raise ValidationError(
-                        {'start_time': _("Start time colides with an exising tour", code='invalid')})
-                if existing_tour.start_time <= end_time <= existing_tour.end_time:
-                    raise ValidationError(
-                        {'end_time': _("Start time colides with an exising tour", code='invalid')})
-                if start_time <= existing_tour.start_time and end_time >= existing_tour.end_time:
-                    raise ValidationError({'start_time': ValidationError(
-                        _('Tour ca\'t encompas existing tour'), code='invalid'), 'end_time': ValidationError(
-                        _('Tour ca\'t encompas existing tour'), code='invalid')})
 
