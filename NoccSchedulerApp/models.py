643fcd8bbec9a74599c70ea0073c6d48c1af0ed7
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-11-09 20:13:02 +0100

    asdas

--- NoccSchedulerApp/models.py
+++ NoccSchedulerApp/models.py
@@ -111,7 +111,7 @@ class Tour(models.Model):
         ('Manufacturing', 'Manufacturing'),
     ]
 
-    type_of_customers = models.ChoiceField(label='Type of Customers', choices=TYPE_OF_CUSTOMERS)
+    type_of_customers = models.CharField(max_length=200, label='Type of Customers', choices=TYPE_OF_CUSTOMERS)
     
     PERSON_ASSIGNED_CHOICES = [
         ('None', 'None'),
@@ -120,7 +120,7 @@ class Tour(models.Model):
         ('Charlie', 'Charlie'),
     ]
     nocc_person_assigned = models.CharField(
-        max_length=30,
+        max_length=50,
         choices=PERSON_ASSIGNED_CHOICES,
         default='None',
     )
