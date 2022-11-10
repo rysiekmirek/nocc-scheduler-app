9c95d60c12f0648d75d94b4577046f1e447b35e0
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-11-10 01:37:36 +0100

    das

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -215,7 +215,7 @@ def new_tour(request):
     location_id = Location.objects.filter(location="Krakow").values()[0]['id']
     context={
         'location_id': location_id,
-        'availability': Availability.objects.filter(avail_date="2022-11-13",location_id=location_id).values(),
+        'availability': Availability.objects.filter(avail_date="2022-11-13",location_id=location_id).values()['time_slots'],
         'form': form,
     }
     return render (request, "new-tour.html", context)
