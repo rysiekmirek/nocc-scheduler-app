5ac5e0da6be6fd948d8eb18e136b514773aef636
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-11-13 20:32:03 +0100

    das

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -186,7 +186,7 @@ def new_tour(request):
         dbentry.save()
         #date_of_tour = datetime.strptime(dbentry.date,"%Y-%M-%D").date
         print (dbentry.date)
-        availability_update=Availability.objects.filter(avail_date=dbentry.date).filter(location=dbentry.location).values()[0]
+        availability_update=Availability.objects.filter(avail_date=dbentry.date).filter(location=dbentry.location).values()[0]['time_slots']
 
         print(availability_update)
 
