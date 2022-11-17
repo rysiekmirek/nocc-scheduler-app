f7677e17f6fb9b6095b3fd12db8300de549ff3bd
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-11-17 01:05:50 +0100

    asadsa

--- NoccSchedulerApp/forms.py
+++ NoccSchedulerApp/forms.py
@@ -28,7 +28,7 @@ class TourForm(ModelForm):
                 'class': 'form-control form-control-sm',
                 'placeholder': field.label,
                 })
-
+    """
     def clean(self):
         cleaned_data = super().clean()
         date = cleaned_data.get('date')
@@ -40,7 +40,7 @@ class TourForm(ModelForm):
         #    self.add_error('end_time', ValidationError(_('End time has to be after start time')))
         if date <= today:
             self.add_error('date', ValidationError(_('Tour cannot be scheduled for the same day or in the past')))
-        """for existing_tour in Tour.objects.filter(location=location):
+        for existing_tour in Tour.objects.filter(location=location):
             if date == existing_tour.date and existing_tour.status != "Rejected":
                 if existing_tour.start_time <= start_time <= existing_tour.end_time:
                     raise ValidationError(
