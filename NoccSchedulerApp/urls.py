91b21a4380839f51e405791ddd6ddf505db0d143
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-12-19 01:10:56 +0100

    sadasd

--- NoccSchedulerApp/urls.py
+++ NoccSchedulerApp/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('feedback/<str:pk>/', views.feedback, name='feedback'),
     path('settings/', views.settings, name='settings'),
     path('avail-times/', views.get_avail_times, name='get_avail_times'),
-    path('.well-known/acme-challenge/kLYGW62Q-00swf8Bl0sJGVXVe4A6_Go6j9ZLX1PcABM',TemplateView.as_view(template_name="kLYGW62Q-00swf8Bl0sJGVXVe4A6_Go6j9ZLX1PcABM", content_type="text/plain")),
-    path('.well-known/acme-challenge/-CtZcMP-IPFgaG05CMqxZKG6WtSkjOGW_ZVQUHLY4wI',TemplateView.as_view(template_name="-CtZcMP-IPFgaG05CMqxZKG6WtSkjOGW_ZVQUHLY4wI", content_type="text/plain")),
+    path('.well-known/acme-challenge/9wgBfm21CxIEoUWkqJXEF-2hiR34wQpLZ1kdcHFQKZc',TemplateView.as_view(template_name="9wgBfm21CxIEoUWkqJXEF-2hiR34wQpLZ1kdcHFQKZc", content_type="text/plain")),
     path('', views.main, name = 'main' ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
