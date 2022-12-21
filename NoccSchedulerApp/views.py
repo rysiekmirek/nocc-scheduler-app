66fd7d39441a1c67717bc08cb1173cf6cbb76b06
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-12-21 02:14:35 +0100

    sadas

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -248,10 +248,10 @@ def get_avail_times(request):
         start_times = []
         end_times =[]
         while entry < avail_end_time:
-            start_times.append(entry)
+            start_times.append(str(entry))
             entry = (datetime.combine(date.today(), entry) + timedelta(minutes=15)).time()
             #entry = entry + timedelta(minutes=15)
-            end_times.append(entry)
+            end_times.append(str(entry))
         print (start_times)
         print (end_times)
         #time_slots = Availability.objects.filter(avail_date=f_date, location_id=f_location).values()[0]['time_slots'].split(',')
