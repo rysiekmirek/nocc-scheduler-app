8ad50325edc0052611ff4b5c7088fffebf6036e6
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-12-22 01:50:16 +0100

    dsa

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -243,14 +243,14 @@ def get_avail_times(request):
         other_tours_that_day = Tour.objects.filter(date=f_date,location=f_location).exclude(status="Rejected").exclude(status="Canceled").values()
         for tour in other_tours_that_day:
             existing_tours_times = []
-            tour_start_time = datetime.strptime(tour['start_time'], "%H:%M").time()
-            tour_end_time = datetime.strptime(tour['end_time'], "%H:%M").time()
+            tour_start_time = tour['start_time']
+            tour_end_time = tour['end_time']
             i= tour_start_time
             while i <= tour_end_time:
                 existing_tours_times.append(i.strftime("%H:%M"))
                 i = (datetime.combine(date.today(), i) + timedelta(minutes=15)).time()
             print (existing_tours_times)
-            
+
         while entry < avail_end_time:
             start_times.append(entry.strftime("%H:%M"))
             entry = (datetime.combine(date.today(), entry) + timedelta(minutes=15)).time()
