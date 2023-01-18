d0faed55a6bd902607b22b4944d86ef68e0e0ff5
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-18 23:45:00 +0100

    rty

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -194,7 +194,6 @@ def feedback(request, pk):
 def new_tour(request):
     if request.method == 'POST':
         form = TourForm(request.POST)
-        print (form.cleaned_data)
         try:
             r=request.POST
             tour_data= r.dict()
