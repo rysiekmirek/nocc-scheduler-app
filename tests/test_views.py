9b0898acc74378c2a352709a6fffc98b8ff63cc1
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-19 21:13:12 +0100

    l';

--- tests/test_views.py
+++ tests/test_views.py
@@ -22,8 +22,8 @@ def test_main_view(rf):
     assert response.status_code == 200
 
 # Test tour_details view
-pk="681b5abf-4df8-40f5-b3a1-b4469e66b11b"
-def test_tour_details(request, pk):
+
+def test_tour_details(request):
     # create test data
     location = Location.objects.create(location='Test Location')
     tour = Tour.objects.create(location=location, nocc_person_assigned='Test Person')
