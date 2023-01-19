ae61cf83eadbfe51bd19991cd1af0a7fc7ebf8d0
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-19 01:04:19 +0100

    yui

--- NoccSchedulerApp/settings.py
+++ NoccSchedulerApp/settings.py
@@ -12,20 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
-
-
-MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
 
 
 # Quick-start development settings - unsuitable for production
