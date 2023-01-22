4db157a866d045c0cf61417610d3082a9de7d084
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-23 00:58:26 +0100

    dsad

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,15 +17,15 @@ def test_user_login(client, django_user_model):
     #response = client.get('/archives')
     assert response.status_code == 302
 
-    def test_user_login_fail(client, django_user_model):
-        login_data = dict (
-            f_username = "test_user",
-            f_password = "test_pass")
-        user = django_user_model.objects.create_user(username=login_data['f_username'], password='wrong_pass')
-        response = client.post("/login/", login_data )
-        # Use this:
-        #client.force_login(user)
-        # Or this:
-        #client.login(username=login_data['username'], password=login_data['password'])
-        #response = client.get('/archives')
-        assert response.status_code == 200
+def test_user_login_fail(client, django_user_model):
+    login_data = dict (
+        f_username = "test_user",
+        f_password = "test_pass")
+    user = django_user_model.objects.create_user(username=login_data['f_username'], password='wrong_pass')
+    response = client.post("/login/", login_data )
+    # Use this:
+    #client.force_login(user)
+    # Or this:
+    #client.login(username=login_data['username'], password=login_data['password'])
+    #response = client.get('/archives')
+    assert response.status_code == 200
