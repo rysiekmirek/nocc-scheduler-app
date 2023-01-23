3cabff34f82dfc5c88e4862aab5fb82e0cbade29
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-23 02:44:08 +0100

    qw

--- tests/test_views.py
+++ tests/test_views.py
@@ -38,7 +38,21 @@ def test_check_admin_access(auth_user):
     assert response.status_code == 200
 
 @pytest.mark.django_db
-def test_add_new_tour(client):
+def test_add_new_tour_correct_data(client):
+    #create test data
+    location = Location.objects.create(location='Krakow')
+    form_data = dict(location=location.id, nocc_person_assigned='Test Person', tour_name='Test tour', date= "2024-01-13", start_time="15:30", end_time='16:00',
+    nocc_personnel_required='Yes', requestor_name='Thomas', requestor_email='thomas@mail.com', poc_name= 'John', poc_email= 'john@akamai.com', cc_this_request_to= 'john2@akamai.com',
+    division= 'Compute', attendees_akamai= '2', attendees_guests= '2', customer_or_group_name= 'test', customers_website='website.com', type_of_customers= 'Manufacturing', 
+    category= 'investors', opportunity_ID= '0', comment= 'aaaa', id='9b31ede7-36ba-44cb-b492-c493fd99daaa')
+
+    response = client.post('/new-tour/', form_data)
+    data_posted_to_db = Tour.objects.all().first()
+    assert data_posted_to_db.poc_name == form_data['poc_name']
+    assert response.status_code == 302
+
+@pytest.mark.django_db
+def test_add_new_tour_wrong_times(client):
     #create test data
     location = Location.objects.create(location='Krakow')
     form_data = dict(location=location.id, nocc_person_assigned='Test Person', tour_name='Test tour', date= "2024-01-13", start_time="15:30", end_time='15:00',
@@ -47,8 +61,5 @@ def test_add_new_tour(client):
     category= 'investors', opportunity_ID= '0', comment= 'aaaa', id='9b31ede7-36ba-44cb-b492-c493fd99daaa')
 
     response = client.post('/new-tour/', form_data)
-    #data = response.data
-    #data_in_db = Tour.objects.all().first()
     assert 'End time has to be after start time' in str(response.content)
-
-    #assert data_in_db.poc_name == form_data['poc_name']
+    assert response.status_code == 200
