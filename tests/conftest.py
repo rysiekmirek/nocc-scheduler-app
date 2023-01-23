164f0a0d6bba3fac68aea07eb3b683505908d94b
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-23 01:46:22 +0100

    asd

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,6 @@ def auth_user(django_user_model):
         f_username = "test_user",
         f_password = "test_pass")
     user = django_user_model.objects.create_user(username=login_data['f_username'], password=login_data['f_password'])
-    #client.post("/login/", login_data )
-    client.force_login(user)
+    client.post("/login/", login_data )
+    #client.force_login(user)
     return client
