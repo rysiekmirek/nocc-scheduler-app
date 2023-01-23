2ae2c7b9a287273f4039f89d2dcb1dda0f229cfc
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-23 01:59:05 +0100

    eqw

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,9 @@
 import pytest
 from django.test import Client
+from datetime import datetime
+from NoccSchedulerApp.models import Tour, Location
+from NoccSchedulerApp.forms import TourFormDetails, TourFormFeedbackDetails
+from NoccSchedulerApp.views import main, tour_details, view_calendar, archives, login_user, logout_user, ask_for_feedback
 
 client=Client()
 pytest_mark = pytest.mark.django_db
@@ -33,3 +37,18 @@ def test_user_login_fail(client, django_user_model):
 def test_check_admin_access(auth_user):
     response = auth_user.get('/archives/')
     assert response.status_code == 200
+
+def test_add_new_tour(client):
+
+    #create test data
+    location = Location.objects.create(location='Krakow')
+    form_data = dict(location=location, nocc_person_assigned='Test Person', tour_name='Test tour', date= datetime.now().date(), start_time="15:30", end_time='16:00',
+    nocc_personnel_required='Yes', requestor_name='Thomas', requestor_email='thomas@mail.com', poc_name= 'John', poc_email= 'john@akamai.com', cc_this_request_to= 'john2@akamai.com',
+    division= 'Compute', attendees_akamai= '2', attendees_guests= '2', customer_or_group_name= 'test', customers_website='website.com', type_of_customers= 'Manufacturing', 
+    category= 'investors', opportunity_ID= '0', comment= 'aaaa', id='9b31ede7-36ba-44cb-b492-c493fd99daaa')
+
+    response = client.post('/new-tour/', form_data)
+    data = response.data
+    data_in_db = Tour.objects.all().first()
+
+    assert data['poc_name'] == form_data['poc_name']
