dcfbd161776e2033cc9eb5653e5517f12ccfab70
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-25 18:16:50 +0100

    sdf

--- NoccSchedulerApp/admin.py
+++ NoccSchedulerApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Tour, Location, Availability
+from .models import Tour, Location, Availability, NoccRepresentatives
 
 admin.site.register(Tour)
 admin.site.register(Location)
 admin.site.register(Availability)
+admin.site.register(NoccRepresentatives)
