2f7ee5b2792ccb8d9d573992051d1d4f75052e02
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-26 22:59:50 +0100

    dfhg

--- NoccSchedulerApp/settings.py
+++ NoccSchedulerApp/settings.py
@@ -152,7 +152,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nvsakamai@gmail.com'
 EMAIL_HOST_PASSWORD = 'vfoetkhbxgxsyjfe'
-EMAIL_PORT = 587
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
 # from django.core.mail import send_mail
