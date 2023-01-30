181560af5c7f1974c64f46788dac6dfd8027aba0
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-30 02:28:36 +0100

    asd

--- NoccSchedulerApp/settings.py
+++ NoccSchedulerApp/settings.py
@@ -147,7 +147,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nvsakamai@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get["gmail_pass"]
+EMAIL_HOST_PASSWORD = os.environ.get('gmail_pass')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
