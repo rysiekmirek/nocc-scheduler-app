b3d5c05689a9bc46fadbe661e0c0de829a6f3a83
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-30 23:35:23 +0100

    qweqw

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -62,7 +62,7 @@ def tour_details(request, pk):
 
     print (initial_tour_data)
     print('-----------------------------------------------------------')
-    print (tour_data.dict())
+    print (vars(tour_data))
     return render(request, "tour-details.html", context)
 
 
