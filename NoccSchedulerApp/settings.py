b4789c90946817d6774a2bde93d2fb38d791e297
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-01-30 03:17:40 +0100

    asd

--- NoccSchedulerApp/settings.py
+++ NoccSchedulerApp/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+load_dotenv(find_dotenv())
 
 
 # Quick-start development settings - unsuitable for production
