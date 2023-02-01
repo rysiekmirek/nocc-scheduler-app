6dbaad1403516ed5dd124f4326bc0023ed7729d7
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-02-01 22:18:03 +0100

    asd

--- NoccSchedulerApp/management/commands/send_feedback_email.py
+++ NoccSchedulerApp/management/commands/send_feedback_email.py
@@ -10,36 +10,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Query the database for all dates in the past
-        tours = Tour.objects.filter(date__lt=timezone.now().date(), feedback_status='No answer 3 days', status="Approved")
-        
-        # subject = '[NOCC-Visit-Scheduler] - Testing crontab - run every 1h'
-        # from_email = 'nvs@akamai.com'
-        # to = ['rmirek@akamai.com']
-        # html_content = f'just to test'
-        # msg = EmailMessage(subject, html_content, from_email, to)
-        # msg.content_subtype = "html"
-        # msg.send()
-
-        # for tour in tours:
-        #     send_email(
-        #         subject = f'[NOCC-Visit-Scheduler] - Please tell us more about Your visit at Akamai NOCC on {tour.date} - it takes just 1 minute to complete', 
-        #         to = [tour.requestor_email, 'rmirek@akamai.com'], 
-        #         html_content = f'<h2>Hi {tour.requestor_name}, </h2><br> Please go to our <br> <a href="http://nvs.akamai.com/feedback/{tour.id}">feedback form </a>' \
-        #                         f'and share Your experience with us')
-        #     print(tour)
-
-        # tours.feedback_status = 'Request sent'
-        # tours.save()
-        # for tour in tours:
-        #     print ('------------------------------------------------------------')
-        #     print (tour.tour_name)
-        #     print(tour.feedback_status)
+        tours = Tour.objects.filter(date__lt=timezone.now().date(), feedback_status='Request not sent', status="Approved")
 
         for tour in tours:
-            print (tour.tour_name)
-            print(tour.feedback_status)
-            print ('------------------------------------------------------------')
-            tour.feedback_status = 'Request not sent'
+            send_email(
+                subject = f'[NOCC-Visit-Scheduler] - Please tell us more about Your visit at Akamai NOCC on {tour.date} - it takes just 1 minute to complete', 
+                to = [tour.requestor_email, 'rmirek@akamai.com'], 
+                html_content = f'<h2>Hi {tour.requestor_name}, </h2><br> Please go to our <br> <a href="http://nvs.akamai.com/feedback/{tour.id}">feedback form </a>' \
+                                f'and share Your experience with us')
+
+            tour.feedback_status = 'Request sent'
             tour.save()
-            print (tour.tour_name)
-            print(tour.feedback_status)
+            print(tour)
