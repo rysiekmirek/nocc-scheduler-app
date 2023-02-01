a07e618504af663d09a502b369b3ee7b07035387
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-02-01 22:01:13 +0100

    fsd

--- NoccSchedulerApp/management/commands/send_feedback_email.py
+++ NoccSchedulerApp/management/commands/send_feedback_email.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.core.mail import EmailMessage
 from django.utils import timezone
 from NoccSchedulerApp.models import Tour
+from NoccSchedulerApp.views import send_email
 
 
 class Command(BaseCommand):
@@ -10,21 +11,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Query the database for all dates in the past
         tours = Tour.objects.filter(date__lt=timezone.now().date(), feedback_status='Request not sent', status="Approved")
-
-        subject = '[NOCC-Visit-Scheduler] - Testing crontab - run every 1h'
-        from_email = 'nvs@akamai.com'
-        to = ['rmirek@akamai.com']
-        html_content = f'just to test'
-        msg = EmailMessage(subject, html_content, from_email, to)
-        msg.content_subtype = "html"
-        msg.send()
+        print (tours)
+        print ('------------------------------------------------------------')
+        # subject = '[NOCC-Visit-Scheduler] - Testing crontab - run every 1h'
+        # from_email = 'nvs@akamai.com'
+        # to = ['rmirek@akamai.com']
+        # html_content = f'just to test'
+        # msg = EmailMessage(subject, html_content, from_email, to)
+        # msg.content_subtype = "html"
+        # msg.send()
 
         # for tour in tours:
-        #     subject = '[NOCC-Visit-Scheduler] - Please tell us more about Your visit at Akamai NOCC on {tour.date} - it takes just 1 minute to complete'
-        #     from_email = 'nvs@akamai.com'
-        #     to = [tour.requestor_email, 'rmirek@akamai.com']
-        #     html_content = f'<h2>Hi {tour.requestor_name}, </h2><br> Please go to our <br> <a href="http://nvs.akamai.com/feedback/{tour.id}">feedback form </a> and share Your feedback with us'
-        #     msg = EmailMessage(subject, html_content, from_email, to)
-        #     msg.content_subtype = "html"
-        #     msg.send()
-        #     print (tour)
+        #     send_email(
+        #         subject = f'[NOCC-Visit-Scheduler] - Please tell us more about Your visit at Akamai NOCC on {tour.date} - it takes just 1 minute to complete', 
+        #         to = [tour.requestor_email, 'rmirek@akamai.com'], 
+        #         html_content = f'<h2>Hi {tour.requestor_name}, </h2><br> Please go to our <br> <a href="http://nvs.akamai.com/feedback/{tour.id}">feedback form </a>' \
+        #                         f'and share Your experience with us')
+        #     print(tour)
+
+        # tours.feedback_status = 'Request sent'
+        # tours.save()
+        # print ('------------------------------------------------------------')
+        # print (tours)
+
