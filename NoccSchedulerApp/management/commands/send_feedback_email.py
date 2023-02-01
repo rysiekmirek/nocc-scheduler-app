4beda3442e10bc7c9314192637eba1b6acb09c49
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-02-01 01:18:45 +0100

    fsdfsd

--- NoccSchedulerApp/management/commands/send_feedback_email.py
+++ NoccSchedulerApp/management/commands/send_feedback_email.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import EmailMessage
 from django.utils import timezone
-from tours.models import Tour
+from .models import Tour
 
 
 class Command(BaseCommand):
