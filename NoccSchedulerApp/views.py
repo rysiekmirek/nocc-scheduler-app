798ba93cc2ac99c2fd4a6c6c855030777a00fdf3
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-02-09 20:38:56 +0100

    asd

--- NoccSchedulerApp/views.py
+++ NoccSchedulerApp/views.py
@@ -396,6 +396,8 @@ def send_email_ics(pk):
     from_email = 'nvs@akamai.com'
     to_email = [tour_data.requestor_email, tour_data.poc_email,
                 tour_data.cc_this_request_to, 'rmirek@akamai.com']
+    if tour_data.custom_welcome_screen_needed == 'Yes':
+        to_email.append('marketing-dummy-test-mail@akamai.com')
 
     html_content = f'<p>Hi {tour_data.requestor_name}, <br>' +\
         f'Your NOCC visit has been approved with the following details:' +\
