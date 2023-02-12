bcdfed6e1440d5963d6f129a97e8153f6fd50c3a
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2023-02-13 00:24:38 +0100

    qwe

--- NoccSchedulerApp/management/commands/send_feedback_email.py
+++ NoccSchedulerApp/management/commands/send_feedback_email.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from datetime import date, timedelta
 from NoccSchedulerApp.models import Tour
 from NoccSchedulerApp.views import send_email
 
@@ -8,13 +9,13 @@ class Command(BaseCommand):
     help = 'Checks the database and sends feedback emails'
 
     def handle(self, *args, **options):
-        # Query the database for all dates in the past
-        tours = Tour.objects.filter(date__lt=timezone.now().date(), feedback_status='Request not sent', status="Approved")
+        # Query the database for all dates earlier than yesterday (UTC)
+        yesterday = date.today() - timedelta(days = 1)
+        tours = Tour.objects.filter(date__lt=yesterday, feedback_status='Request not sent', status="Approved")
 
         for tour in tours:
             send_email(template='feedback_form', tour_data=tour)
             tour.feedback_status = 'Request sent'
             tour.save()
-            print(tour)
 
-        send_email(template = 'custom', tour_data=tours.first(), subject='Send_feedback_email just ran', html_content=f'{tours}', to=['rmirek@akamai.com'])
+        send_email(template = 'custom', tour_data=tours.first(), subject='Send_feedback_email just ran', html_content=f'<h1>{tours}</h1>', to=['rmirek@akamai.com'])
